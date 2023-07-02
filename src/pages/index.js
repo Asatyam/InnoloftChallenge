8d810c88550561cfb828301f51e55ccea6de14ab
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-07-02 17:33:21 +0530

    Set up the home page for the website

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,33 @@ import Nav from '@/components/Nav';
 config();
 
 export default function Home({ config }) {
-
   return (
-    <div>
-      <Nav config = {config}/>
-    </div>
+    <main>
+      <Nav config={config} />
+      <h1 className='font-bold text-4xl p-4 text-center'>This is a website made for the Innoloft frontend challenge</h1>
+      <article className='p-2 m-8 border-2' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore a hic accusamus aliquam nulla labore nisi est, provident alias ullam delectus molestias rem dignissimos tempore, debitis corrupti! Soluta, reprehenderit?
+       <br/> 
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, doloribus qui corporis iure suscipit quasi sapiente accusantium labore repudiandae earum ea impedit voluptatum necessitatibus iusto ad, reiciendis dolores debitis praesentium.
+      </article>
+      <article className='p-2 m-8 border-2' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore a hic accusamus aliquam nulla labore nisi est, provident alias ullam delectus molestias rem dignissimos tempore, debitis corrupti! Soluta, reprehenderit?
+       <br/> 
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, doloribus qui corporis iure suscipit quasi sapiente accusantium labore repudiandae earum ea impedit voluptatum necessitatibus iusto ad, reiciendis dolores debitis praesentium.
+      </article>
+      <article className='p-2 m-8 border-2' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam inventore a hic accusamus aliquam nulla labore nisi est, provident alias ullam delectus molestias rem dignissimos tempore, debitis corrupti! Soluta, reprehenderit?
+       <br/> 
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, doloribus qui corporis iure suscipit quasi sapiente accusantium labore repudiandae earum ea impedit voluptatum necessitatibus iusto ad, reiciendis dolores debitis praesentium.
+      </article>
+    </main>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await axios.get(`https://api-test.innoloft.com/configuration/${process.env.APP_ID || 1}`);
+  const res = await axios.get(
+    `https://api-test.innoloft.com/configuration/${process.env.APP_ID || 1}`
+  );
   const config = res.data;
 
   return {
