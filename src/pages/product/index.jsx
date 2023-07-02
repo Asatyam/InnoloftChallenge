0a5c7cb2f1ede3cc4dd39ab2a8f3c04ef10370e9
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-07-02 18:12:09 +0530

    Display spinner on loading

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,24 +1,53 @@
-import React, { useEffect } from "react";
-import { productSlice,selectProduct,fetchProduct  } from "@/store/product/productSlice";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Suspense, useEffect } from 'react';
+import {
+  productSlice,
+  selectProduct,
+  fetchProduct,
+} from '@/store/product/productSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import AtomicSpinner from 'atomic-spinner';
+import Nav from '@/components/Nav';
+import axios from 'axios';
 
-export default function ProductView(){
+export default function ProductView({config}) {
+  const dispatch = useDispatch();
+  const product = useSelector(selectProduct);
 
-    const dispatch = useDispatch();
-    const product = useSelector(selectProduct);
+  const status = useSelector((state) => state.product.status);
+  const error = useSelector((state) => state.product.error);
 
-    const status = useSelector(state=>state.product.status);
-    const error = useSelector(state=>state.product.error);
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchProduct());
+    }
+  }, [status, dispatch]);
 
-    useEffect(()=>{
-        if (status === 'idle'){
-            dispatch(fetchProduct());
-        }
-    },[status, dispatch])
-    return(
-        <div>
-            {product.status}
-            hello
-        </div>
-    )
+  if (status == 'loading') {
+    return (
+      <>
+      <Nav config={config}/>
+      <div className='flex justify-center items-center'>
+        <AtomicSpinner />
+      </div>
+      </>
+    );
+  }
+  return (
+    <main>
+      <Nav config={config}/>
+      {product.status}
+      hello
+    </main>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await axios.get(
+    `https://api-test.innoloft.com/configuration/${process.env.APP_ID || 1}`
+  );
+  const config = res.data;
+
+  return {
+    props: { config },
+  };
+};
