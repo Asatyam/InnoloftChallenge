f8becc554c5ebb9c8e65a453e93755e0216ba480
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-07-03 13:18:48 +0530

    Define all handler functions

--- src/pages/product/edit.jsx
+++ src/pages/product/edit.jsx
@@ -25,12 +25,12 @@ export default function ProductEdit({config, trlOptions}) {
   const [models, setModels] = useState([]);
   const [trl ,setTrl] = useState('');
 
-  
-
   const [category, setCategory] = useState('');
   const [model, setModel] = useState('');
 
   const[ isDone, setIsDone] = useState(false);
+
+  const [id, setId] = useState(3);
 
   useEffect(() => {
     if (status === 'idle') {
@@ -44,6 +44,7 @@ export default function ProductEdit({config, trlOptions}) {
       setDescription(product.description);
       setModels(product.businessModels);
       setTrl(product.trl);
+
       setIsDone(true);
     }
   },[status, product,isDone]);
@@ -57,6 +58,26 @@ export default function ProductEdit({config, trlOptions}) {
       </div>
       </>
     );
+  }
+
+  const handleReset = (e)=>{
+
+    setCategories(product.categories);
+    setDescription(product.description);
+    setModels(product.businessModels);
+    setTrl(product.trl);
+  }
+
+  const deleteModel = (e)=>{
+      const mId = Number(e.target.id);
+   const modified =  models.filter(model=>model.id !== mId);
+    setModels(modified);
+
+  }
+  const deleteCategory = (e)=>{
+    const cId = Number(e.target.id);
+   const modified =  categories.filter(category=>category.id !== cId);
+    setCategories(modified);
   }
   
   return (
@@ -75,12 +96,12 @@ export default function ProductEdit({config, trlOptions}) {
               return(
                 <div key={category.id} className='flex '>
                 <p className='bg-cyan-600 text-sm p-1 text-white rounded-lg  m-2 w-fit'>{category.name}  </p>
-                <button className=' w-6 h-6 m-2 '><img className='h-full'  src='/delete.png' alt='delete icon '/></button>
+                <button onClick={deleteCategory}  className=' w-6 h-6 m-2 '><img className='h-full' id={category.id}  src='/delete.png' alt='delete icon '/></button>
                 </div>
               )
             })}
             <input placeholder='Add more categories' className=' bg-cyan-900 border-cyan-400 border-2 px-2 p-1 m-2 rounded text-white outline-none'  value={category} onChange={e=>setCategory(e.target.value)}/>
-            <button className=' px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white mx-auto'> Add</button>
+            <button className=' px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white mx-auto' onClick={(e)=>{setCategories(c=>[...categories,{id,name:category} ]); setId(id+1);setCategory('')}}> Add</button>
           </div>
           <div  className='p-2 border-2 m-2 '>
             <label className='font-semibold' htmlFor='models'>Business Models</label>
@@ -88,12 +109,12 @@ export default function ProductEdit({config, trlOptions}) {
               return(
                 <div key={model.id} className='flex'>
                 <p className='bg-cyan-600 text-sm p-1 text-white rounded-lg  m-2 w-fit'>{model.name}</p>
-                <button className=' w-6 h-6 m-2 '><img className='h-full'  src='/delete.png' alt='delete icon '/></button>
+                <button className=' w-6 h-6 m-2 ' onClick={deleteModel} ><img className='h-full' id={model.id} src='/delete.png' alt='delete icon '/></button>
                 </div>
               )
             })}
             <input placeholder='Add More Models' className=' bg-cyan-900 border-cyan-400 border-2 px-2 p-1 m-2 rounded text-white outline-none' value={model} onChange={e=>setModel(e.target.value)}/>
-            <button className=' px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white mx-auto'> Add</button>
+            <button className=' px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white mx-auto' onClick={(e)=>{setModels(m=>[ ...models,{id,name:model}]); setId(id+1);setModel('')}}> Add</button>
           </div>
           <div  className='p-2 border-2  m-2 flex flex-col gap-4 ' >
              <label className='font-semibold ' htmlFor='trl'>TRL</label>
@@ -102,7 +123,11 @@ export default function ProductEdit({config, trlOptions}) {
               {trlOptions.map(trl=>(<option key={trl.id}>{trl.name}</option>))}
               </select>
           </div>
-            <button type='submit' className='p-1 px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white mx-auto '>Submit</button>
+
+              <div className='flex justify-evenly '>
+                <button type='submit' className='p-1 px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white  '>Submit</button>
+                <button onClick={handleReset} className='p-1 px-2 text-lg font-semibold hover:bg-indigo-900 transition  text-center border-2 border-cyan-300 bg-indigo-500 text-white  '>Reset</button>
+              </div>
         </form>
         </main>
     </main>
